a031590a47df7ae3531852195205c18f44127d96
Dhuta Pratama <me@dhutapratama.com>
2016-02-17 04:43:13 +0700

    Initial Commit Codeigniter 3.0.4

---
+++ application/controllers/Api_apps.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api_apps extends CI_Controller {
+
+	public function __construct() {
+        parent::__construct();
+        //$this->load->library();
+     }
+
+	public function index() {
+		$this->load->view('make_json');
+	}
+}
---
+++ application/views/make_json.php
@@ -0,0 +1,85 @@
+<?php
+	header("Access-Control-Allow-Methods: GET, OPTIONS, POST, PUT, DELETE");
+	header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding");
+	if(isset($_SERVER['HTTP_ORIGIN'])) {
+		header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+	}
+	header('Access-Control-Allow-Credentials: true');
+	header('Content-Type: application/json');
+
+    $tab = "  ";
+    $new_json = "";
+    $indent_level = 0;
+    $in_string = false;
+
+    $json_obj = $data;
+
+    if($json_obj === false)
+        return false;
+
+    $json = json_encode($json_obj);
+    $len = strlen($json);
+
+    for($c = 0; $c < $len; $c++)
+    {
+        $char = $json[$c];
+        switch($char)
+        {
+            case '{':
+            case '[':
+                if(!$in_string)
+                {
+                    $new_json .= $char . "\n" . str_repeat($tab, $indent_level+1);
+                    $indent_level++;
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case '}':
+            case ']':
+                if(!$in_string)
+                {
+                    $indent_level--;
+                    $new_json .= "\n" . str_repeat($tab, $indent_level) . $char;
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case ',':
+                if(!$in_string)
+                {
+                    $new_json .= ",\n" . str_repeat($tab, $indent_level);
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case ':':
+                if(!$in_string)
+                {
+                    $new_json .= ": ";
+                }
+                else
+                {
+                    $new_json .= $char;
+                }
+                break;
+            case '"':
+                if($c > 0 && $json[$c-1] != '\\')
+                {
+                    $in_string = !$in_string;
+                }
+            default:
+                $new_json .= $char;
+                break;                   
+        }
+    }
+
+    echo $new_json;
+    exit();
+?>
