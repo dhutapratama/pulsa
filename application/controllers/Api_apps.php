9f274f26323bac2fc6183498e7d348e524fc4f6a
Dhuta Pratama <me@dhutapratama.com>
2016-02-24 14:01:06 +0700

    tab 2

--- application/controllers/Api_apps.php
+++ application/controllers/Api_apps.php
@@ -5,7 +5,7 @@ class Api_apps extends CI_Controller {
 
 	private $consumer_key 	= 'dj0yJmk9VXpraU05Tko3NGNpJmQ9WVdrOWVUaE5Tbmw0TTJNbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1kNg--';
 	private $secret_key 	= '13b7d68db3f35f230363fdc9ae14d6aad5d3db5a';
-	private $ym_center		= 'mister2h2';
+	private $ym_center		= 'dutasms';
 
 	public function __construct() {
         parent::__construct();
