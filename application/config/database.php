8bf6d1425405a724332d7d4c406f8d54d895d3f4
Dhuta Pratama <me@dhutapratama.com>
2016-03-07 21:58:17 +0700

    Updates

--- application/config/database.php
+++ application/config/database.php
@@ -4,13 +4,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if ($_SERVER['HTTP_HOST'] == 'ayoisipulsa.app'| $_SERVER['HTTP_HOST'] == 'www.ayoisipulsa.app') {
+if ($_SERVER['HTTP_HOST'] == 'ayoisipulsa.app' || $_SERVER['HTTP_HOST'] == 'www.ayoisipulsa.app') {
 	$db['default'] = array(
 		'dsn'	=> '',
 		'hostname' => 'localhost',
 		'username' => 'z',
 		'password' => 'z',
 		'database' => 'zadmin_ayoisipulsa',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+	);
+} elseif ($_SERVER['HTTP_HOST'] == 'pulsa.freevar.com' || $_SERVER['HTTP_HOST'] == 'www.pulsa.freevar.com') {
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'localhost',
+		'username' => '943127',
+		'password' => '48624862',
+		'database' => '943127',
 		'dbdriver' => 'mysqli',
 		'dbprefix' => '',
 		'pconnect' => FALSE,
